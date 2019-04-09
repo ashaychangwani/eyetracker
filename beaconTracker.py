faf2d6a05806147958d93502990005e09a7cd130
Ashay Changwani <ashay@Ashays-iMac.local>
2019-04-09 23:49:47 +0530

    Beacon tracker functional

---
+++ beaconTracker.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 22:11:04 2019
+
+@author: ashay
+"""
+import cv2
+import numpy as np
+cap = cv2.VideoCapture(1)
+
+pts = []
+while (1):
+
+    # Take each frame
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#    hsv=cv2.adaptiveThreshold(hsv,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,21,2)
+#
+    lower_white = np.array([180])
+    upper_white = np.array([255])
+    mask = cv2.inRange(hsv, lower_white, upper_white)
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask)
+    
+    contours, heih = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+       # calculate moments for each contour
+       M = cv2.moments(c)
+     
+       # calculate x,y coordinate of center
+       if M["m00"] != 0:
+         cX = int(M["m10"] / M["m00"])
+         cY = int(M["m01"] / M["m00"])
+       else:
+         cX, cY = 0, 0
+       cv2.circle(mask, (cX, cY), 15, (120, 120, 120), 2)
+
+
+#    cv2.circle(hsv, maxLoc, 15, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.imshow('Track Laser', mask)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- interface.py
+++ interface.py
@@ -72,6 +72,14 @@ from tkinter import *
 import PIL
 from PIL import Image,ImageTk
 import cv2
+import tensorflow as tf
+import sys
+sys.path.append("/Users/ashay/Desktop/eyeTracker/models/research/object_detection")
+sys.path.append("/Users/ashay/Desktop/eyeTracker/models/research")
+
+
+
+
 
 camFrameWidth=1020
 menuFrameWidth=1920-camFrameWidth
@@ -165,4 +173,166 @@ startCamera.place(x=menuFrameWidth/2,y=menuFrameHeight/2,anchor="center")
 #except:
 #    pass
 root.mainloop()
+cap=None
+cap2=None
+
+
+
+
+
+'''
+
+https://www.youtube.com/watch?v=kbdbZFT9NQI
+from tkinter import *
+import PIL
+from PIL import Image,ImageTk
+import cv2
+import tensorflow as tf
+import sys
+cap=cv2.VideoCapture(1)
+while True:
+    ret,frame=cap.read()
+    roi=frame
+    img_inv=cv2.bitwise_not(frame)
+    gray_roi= cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
+    hsv= cv2.cvtColor(img_inv, cv2.COLOR_BGR2HSV) 
+    h,s,v = cv2.split(hsv)
+    
+    _, threshold = cv2.threshold(v, 130 , 255,cv2.THRESH_BINARY_INV)
+    
+    cv2.imshow("GROI",threshold)
+    
+    key=cv2.waitKey(30)
+    if key==27:
+        break
+cv2.destroyAllWindows()
+
+
+
+
+
+MY PERSONAL TRAINING THING
+
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+import numpy as np
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile("outputFolder/frozen_inference_graph.pb", 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+label_map = label_map_util.load_labelmap("images/data/object-detection.pbtxt")
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+cap=cv2.VideoCapture(1)
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    while True:
+      ret, image_np = cap.read()
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      # Actual detection.
+      (boxes, scores, classes, num_detections) = sess.run(
+          [boxes, scores, classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      print(scores)
+      # Visualization of the results of a detection.
+      if scores[0][0]>0.8:
+          vis_util.visualize_boxes_and_labels_on_image_array(
+              image_np,
+              np.squeeze(boxes),
+              np.squeeze(classes).astype(np.int32),
+              np.squeeze(scores),
+              category_index,
+              use_normalized_coordinates=True,
+              line_thickness=8)
+
+      cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+      if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
+
+
+
+
+
+
+
+
+
+
+
+#convert xml to csv
+
+
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+ 
+ 
+def xml_to_csv(path):
+    xml_list = []
+    print(path + '/*.xml')
+    for f in glob.glob(path):
+        print(f)
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+ 
+ 
+def main():
+    for directory in ['train','test']:
+        image_path = "/Users/ashay/Desktop/eyeTracker/irisImages/"+directory
+        print(image_path)
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv('data/{}_labels.csv'.format(directory), index=None)
+        print('Successfully converted xml to csv.')
+ 
+ 
+main()
+
+
+#Code to delete all files that don't have XML Equivalent
+files=[]
+for f in glob.glob("*.xml"):
+    files.append(f)
+s=""
+for f in glob.glob("*.jpg"):
+    s=f[:-3]+"xml"
+    if s not in files:
+        os.remove(f)
+'''
 
